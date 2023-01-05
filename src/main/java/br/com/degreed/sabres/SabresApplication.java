8a71247cb306047213817309c17942ec03eea43f
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-01-05 18:21:56 -0300

    Iniciando o Projeto

---
+++ src/main/java/br/com/degreed/sabres/SabresApplication.java
@@ -0,0 +1,13 @@
+package br.com.degreed.sabres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/degreed/sabres/SabresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.degreed.sabres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
